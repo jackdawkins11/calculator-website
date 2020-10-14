73b081aaca068e4c184cbce3bb3392547645c3a2
Jack Dawkins <jackdawkins1974@gmail.com>
2020-10-14 16:21:19 -0500

    changed html calculator to be generated by react

--- frontend/react/app.js
+++ frontend/react/app.js
@@ -1,25 +1,76 @@
 'use strict';
 
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    let liked = this.state.liked;
-
-    let message = liked ? 'You like this' : 'You don\' like this';
-
-    return (
-      <button onClick={() => this.setState({ liked: !liked })}>
-        {message}
-      </button>
-    );
-  }
+function CalculatorScreen(props){
+  return (
+    <div className="calculator-top">
+    {props.screenValue}
+    </div>
+  );
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+function CalculatorButton(props){
+  return (
+    <div className="calculator-button-wrapper">
+      <div className="calculator-button">
+        {props.calculatorButtonValue}
+      </div>
+    </div>
+  );
+}
+
+function CalculatorEqualButton(props){
+  return (
+    <div className="calculator-button-wrapper equal-button-wrapper">
+      <div className="calculator-button equal-button">
+        {props.calculatorButtonValue}
+      </div>
+    </div>
+  );
+}
+
+/* Buttons on calculator. Stored as [ column1, column2, column3, column4 ] */
+let calculatorButtonValues =
+  [ ['+', '7', '4', '1', '0'],
+    ['-', '8', '5', '2', '.' ],
+    ['*', '9', '6', '3', 'c' ],
+    ['%', '='] ];
+
+function Calculator(props){
+  let columns = [];
+  //Add the first 3 columns
+  for( let c=0; c < 3; c++ ){
+    let column = [];
+    for( let r=0; r < 5; r++ ){
+      column.push(
+        <CalculatorButton
+        calculatorButtonValue={calculatorButtonValues[c][r]} />
+        );
+    }
+    columns.push( <div className="calculator-column">{column}</div> );
+  }
+  //Add the last column
+  let column = [
+      <CalculatorButton 
+      calculatorButtonValue={calculatorButtonValues[3][0]} />,
+      <CalculatorEqualButton
+      calculatorButtonValue={calculatorButtonValues[3][1]} />
+    ];
+  columns.push( <div className="calculator-column">{column}</div> );
+  return (
+    <div className="calculator">
+      <CalculatorScreen screenValue={props.screenValue} />
+      <div className="calculator-bottom">
+        {columns}
+      </div>
+    </div>
+  );
+}
+
+function Simple(props){
+  return (
+    <div>Hello world</div>
+  );
+}
+
+ReactDOM.render(<Calculator screenValue={0}/>, document.getElementById("root"));
+//ReactDOM.render(<Simple/>, document.getElementById("root"));
