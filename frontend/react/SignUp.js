2a05b363db2533596ce693fd261675a151ec3f0c
Jack Dawkins <jackdawkins1974@gmail.com>
2020-10-16 14:49:29 -0500

    Added functionality to SignUp page

--- frontend/react/SignUp.js
+++ frontend/react/SignUp.js
@@ -7,7 +7,7 @@ function SignUpUsername( props ){
                 <div className="sign-up-username-label">
                     Username
                 </div>
-                <input className="sign-up-username-input"></input>
+                <input className="sign-up-username-input" onChange={props.onChange} ></input>
             </div>
         </div>
     );
@@ -20,7 +20,7 @@ function SignUpPassword( props ){
                 <div className="sign-up-password-label">
                     Password
                 </div>
-                <input className="sign-up-password-input"></input>
+                <input className="sign-up-password-input" onChange={props.onChange} ></input>
             </div>
         </div>
     );
@@ -33,7 +33,7 @@ function SignUpPasswordConfirm( props ){
                 <div className="sign-up-password-label">
                     Confirm Password
                 </div>
-                <input className="sign-up-password-input"></input>
+                <input className="sign-up-password-input" onChange={props.onChange} ></input>
             </div>
         </div>
     );
@@ -42,7 +42,7 @@ function SignUpPasswordConfirm( props ){
 function SignUpButton( props ){
     return (
         <div className="sign-up-button-wrapper">
-            <div className="sign-up-button">
+            <div className="sign-up-button" onClick={props.onClick} >
                 Sign Up
             </div>
         </div>
@@ -52,17 +52,77 @@ function SignUpButton( props ){
 class SignUp extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            username: "",
+            password: "",
+            confirmPassword: ""
+        };
     }
     render(){
         return (
             <div className="sign-up">
                  {this.props.tabBar}
-                <SignUpUsername />
-                <SignUpPassword />
-                <SignUpPasswordConfirm />
-                <SignUpButton />
+                <SignUpUsername onChange={ (event) => this.changeUsername(event) } />
+                <SignUpPassword onChange={ (event) => this.changePassword(event) } />
+                <SignUpPasswordConfirm onChange={ (event) => this.changeConfirmPassword(event) } />
+                <SignUpButton onClick={ () => this.signUp() } />
             </div>
         );
+    }
+    changeUsername( event ){
+        this.setState({
+            username: event.target.value
+        });
+    }
+    changePassword( event ){
+        this.setState({
+            password: event.target.value
+        });
+    }
+    changeConfirmPassword( event ){
+        this.setState({
+            confirmPassword: event.target.value
+        });
+    }
+    signUp(){
+        let username = this.state.username,
+            password = this.state.password,
+            confirmPassword = this.state.confirmPassword;
+        if( username === "" || password === "" ){
+            this.error( "All fields must be used" );
+        }else if( password !== confirmPassword ){
+            this.error( "Passwords must match" );
+        }else{
+            this.signUpRequest( username, password );
+        }
+    }
+    signUpRequest( username, password ){
+        let credentials = new URLSearchParams();
+        credentials.append( "username", username );
+        credentials.append( "password", password );
+        fetch( "../backend/createAccount.php", {
+            method: "POST",
+            body: credentials
+        }).then( (response) => response.json() )
+        .then( result => {
+            if( !result.error ){
+                if( result.available ){
+                    this.success();
+                }else{
+                    this.error( "That username is in use" );
+                }
+            }else{
+                this.error( "There was an error" );
+            }
+        }).catch( (reason) => {
+            this.error( "There was an error" );
+        });
+    }
+    success(){
+        console.log( "Successfully created account" ); 
+    }
+    error( message ){
+        console.log( "Error: " + message );
     }
 }
 
